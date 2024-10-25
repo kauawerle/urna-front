a9a4bf27c3814fa365d3e98a8fb6a73a5dbb92cb
kauawerle <kauawerle19@gmail.com>
2024-10-25 18:29:56 -0300

    implement new pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './layouts/login/login.component';
+import { MainComponent } from './layouts/main/main.component';
+import { RegisterCandidatoComponent } from './layouts/register-candidato/register-candidato.component';
+import { VotarComponent } from './layouts/votar/votar.component';
+import { ApuracaoComponent } from './layouts/apuracao/apuracao.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'home',
+    component: MainComponent,
+    children: [
+      {  path: 'home/candidato', component: RegisterCandidatoComponent},
+      {  path: 'home/votar', component: VotarComponent},
+      {  path: 'home/apuracao', component: ApuracaoComponent},
+    ]
+  }
+];
---
+++ src/app/layouts/apuracao/apuracao.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apuracao',
+  standalone: true,
+  imports: [],
+  templateUrl: './apuracao.component.html',
+  styleUrl: './apuracao.component.scss'
+})
+export class ApuracaoComponent {
+
+}
