422f0a63502c3ebcb334a9b6a47a90692cc70d1e
wtfpac <kwellxz@gmail.com>
2024-10-25 21:18:45 -0300

    adicionado route eleitor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { MainComponent } from './layouts/main/main.component';
 import { RegisterCandidatoComponent } from './layouts/register-candidato/register-candidato.component';
 import { VotarComponent } from './layouts/votar/votar.component';
 import { ApuracaoComponent } from './layouts/apuracao/apuracao.component';
+import { CandidatoListComponent } from './layouts/candidato-list/candidato-list.component';
 import { EleitorFormComponent } from './layouts/eleitor/eleitor-form/eleitor-form.component';
 import { EleitorListComponent } from './layouts/eleitor/eleitor-list/eleitor-list.component';
 
@@ -14,13 +15,17 @@ export const routes: Routes = [
     path: 'home',
     component: MainComponent,
     children: [
-      { path: 'home/eleitor', children: [
-        {path: 'eleitor/eleitor-register', component: EleitorFormComponent},
-        {path: 'eleitor/eleitor-list', component: EleitorListComponent}
-      ]},
-      {  path: 'home/candidato', component: RegisterCandidatoComponent},
-      {  path: 'home/votar', component: VotarComponent},
-      {  path: 'home/apuracao', component: ApuracaoComponent},
+      {
+        path: 'candidato', component: CandidatoListComponent, children: [
+          { path: 'candidato-form', component: RegisterCandidatoComponent },
+        ],
+      },
+      {
+        path: 'eleitor', component: EleitorListComponent, children: [
+        { path: 'eleitor-form', component: EleitorFormComponent},
+      ] },
+      { path: 'votar', component: VotarComponent },
+      { path: 'apuracao', component: ApuracaoComponent },
     ]
   }
 ];
